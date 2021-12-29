5730fed5eaeb113da4cc2d1eff11becaafec6e0f
Dom B <domlamont@yahoo.com>
2021-12-29 11:13:21 -0800

    Created a handleChange Func in Login.jsx

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,11 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = (props) => {
+    const [credentials, setCredentials] = useState({username: "", password: ""});
+
+    const handleChange = (event) => {
+        let input = {username: credentials.username, password: credentials.password};
+
+        if(event.target.name === "username"){
+            input = {
+                username: event.target.value,
+                password: credentials.password
+            }
+        }else if(event.target.name === "password"){
+            input = {
+                username: credentials.username,
+                password: event.target.value
+            }
+        }
+        
+        setCredentials(input);
+    };
+
+    console.log(credentials);
+
     return(
         <div>
-            <main>
-                <textarea>...Some Text</textarea>
-            </main>
+            <form>
+                <label>
+                    Username: 
+                    <input 
+                        type="text" 
+                        name="username" 
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>
+                    Password:
+                    <input 
+                        type="password" 
+                        name="password" 
+                        onChange={handleChange}
+                    />
+                </label>
+                <button type="submit">Login</button>
+            </form>
         </div>
     );
 };
